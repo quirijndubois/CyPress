342c4fc44a61c345f7addea415c0968b98164546
Jupieterr <pieterteb@gmail.com>
2024-04-01 12:12:33 +0200

    Added some useful files for pdf testing

--- PDF_testing/main.c
+++ PDF_testing/main.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
+#include <string.h>
 
 enum OBJ_type;
 struct PDF_object;
 struct PDF_file;
 
 int main(void) {
+    FILE* source = fopen("test.txt", "r");
+    FILE* pdf = fopen("test.pdf", "w");
+    
+    char line[256] = { 0 };
+    while(fgets(line, 256, source)) {
+        fputs(line, pdf);
+    }
 
+    fclose(source);
+    fclose(pdf);
 
     return 0;
 }
