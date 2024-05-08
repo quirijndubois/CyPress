c03be665b2fb8a43c104255379ca4d10c742bb6e
Jupieterr <pieterteb@gmail.com>
2024-05-08 16:32:49 +0200

    Moved some stuff from pdf.h to pdf.c

--- PDF_testing/pdf.h
+++ PDF_testing/pdf.h
@@ -1,15 +1,6 @@
 #ifndef PDF_H
 #define PDF_H
 
-enum ObjectType;
-
-struct PDF_info {
-    void;
-};
-
-struct PDF_object;
-struct PDF_document;
-
 #define PDF_VERSION "1.7" // Should be determined automatically, need more research for that.
 
 #endif
