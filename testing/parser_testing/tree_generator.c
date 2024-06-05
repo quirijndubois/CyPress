302122e1ce0c274fe0a746fe4803051d71271be2
Quirijn du Bois <quirijndubois@gmail.com>
2024-06-05 23:43:24 +0200

    commit

--- testing/parser_testing/tree_generator.c
+++ testing/parser_testing/tree_generator.c
@@ -1,4 +1,6 @@
 void generate_tree(char** tokens){
+    // Not implemented! 
+    // Right now just printing the tokens
     int i = 0;
     while (tokens[i]!=NULL) {
         printf("%s\n", tokens[i]);
