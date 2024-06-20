1036e023deb4f7364f0bc04dbc4e406bd3e44d57
Jupieterr <pieterteb@gmail.com>
2024-06-20 13:29:48 +0200

    Improved invalid object class checking for creating object headers.

--- testing/PDF_testing/CyPDF/cypdf_object.c
+++ testing/PDF_testing/CyPDF/cypdf_object.c
@@ -3,7 +3,7 @@
 
 
 
-CYPDF_Obj_Header* CYPDF_Obj_Header_New(CYPDF_BOOL direct, CYPDF_BOOL indirect, CYPDF_UINT32 ID, CYPDF_UINT16 obj_gen, CYPDF_UINT16 obj_class) {
+CYPDF_Obj_Header* CYPDF_Obj_Header_New(CYPDF_BOOL direct, CYPDF_BOOL indirect, CYPDF_UINT32 ID, CYPDF_UINT16 obj_gen, CYPDF_UINT16 obj_class, CYPDF_UINT16 obj_subclass) {
     CYPDF_Obj_Header* obj_header = malloc(sizeof(CYPDF_Obj_Header));
     direct = !!direct;
     indirect = !!indirect;
@@ -14,11 +14,15 @@ CYPDF_Obj_Header* CYPDF_Obj_Header_New(CYPDF_BOOL direct, CYPDF_BOOL indirect, C
     }
     obj_header->obj_id = direct * CYPDF_OTYPE_DIRECT | indirect * CYPDF_OTYPE_INDIRECT | ID;
     obj_header->obj_gen = obj_gen;
-    if (!(obj_class & CYPDF_OCLASS_ANY)) {
+    if (obj_class > CYPDF_OCLASS_COUNT) {
         printf("Unknown object class.\n");
         obj_class = CYPDF_OCLASS_UNKNOWN;
     }
-    obj_header->obj_class = obj_class;
+    if (obj_subclass & CYPDF_OCLASS_ANY || (obj_subclass >> 8) > CYPDF_OSUBCLASS_COUNT) {
+        printf("Unknown object subclass.\n");
+        obj_subclass = CYPDF_OSUBCLASS_UNKNOWN;
+    }
+    obj_header->obj_class = obj_class | obj_subclass;
 
     return obj_header;
 }
--- testing/PDF_testing/CyPDF/cypdf_object.h
+++ testing/PDF_testing/CyPDF/cypdf_object.h
@@ -11,6 +11,7 @@
 #define CYPDF_OTYPE_INDIRECT        0x40000000
 #define CYPDF_OTYPE_ANY             (CYPDF_OTYPE_DIRECT) | (CYPDF_OTYPE_INDIRECT)
 
+#define CYPDF_OCLASS_NONE           0x0000
 #define CYPDF_OCLASS_UNKNOWN        0x0001
 #define CYPDF_OCLASS_NULL           0x0002
 #define CYPDF_OCLASS_BOOL           0x0003
@@ -21,11 +22,15 @@
 #define CYPDF_OCLASS_ARRAY          0x0008
 #define CYPDF_OCLASS_DICT           0x0009
 #define CYPDF_OCLASS_STREAM         0x000A      /* a stream is always an indirect object */
+#define CYPDF_OCLASS_COUNT          0x000A
 
 #define CYPDF_OCLASS_ANY            0x00FF
 
-#define CYPDF_OCUBCLASS_UNKNOWN     0x0100
-#define CYPDF_OSUBCLASS_HEADER      0x0200
+#define CYPDF_OSUBCLASS_NONE        0x0000
+#define CYPDF_OSUBCLASS_UNKNOWN     0x0100
+#define CYPDF_OSUBCLASS_COUNT       0x0001
+
+#define CYPDF_OSUBCLASS_ANY         0xFF00
 
 
 /* object_id structure:
@@ -48,7 +53,7 @@ typedef struct _CYPDF_Obj_Header {
 typedef void CYPDF_Object;
 
 
-CYPDF_Obj_Header* CYPDF_Obj_Header_New(CYPDF_BOOL direct, CYPDF_BOOL indirect, CYPDF_UINT32 obj_id, CYPDF_UINT16 obj_gen, CYPDF_UINT16 obj_class);
+CYPDF_Obj_Header* CYPDF_Obj_Header_New(CYPDF_BOOL direct, CYPDF_BOOL indirect, CYPDF_UINT32 obj_id, CYPDF_UINT16 obj_gen, CYPDF_UINT16 obj_class, CYPDF_UINT16 obj_subclass);
 
 void CYPDF_Obj_Header_Free(CYPDF_Obj_Header* obj_header);
 
