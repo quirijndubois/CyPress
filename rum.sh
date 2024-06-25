7dadab6add3c333c3cda8ad6c56a6a77f0e0fe4c
Jupieterrr <pieterteb@gmail.com>
2024-06-25 14:40:14 +0200

    modified cypdf compilation script.

--- rum.sh
+++ rum.sh
@@ -1,10 +1,8 @@
-# Simple compilation bash script (no windows)
-# Made for rapid compiling and testing
+# Compilation bash script for CyPDF
 
 source="CyPDF/src/*.c"
 gccFlags="-fdiagnostics-color=always -Wall -Wextra -Wpedantic -Werror -Wshadow -Wformat=2 -Wconversion -Wunused-parameter"
 
-echo
 gcc -ICyPDF/include $gccFlags $source
 ./a.out
 rm ./a.out
