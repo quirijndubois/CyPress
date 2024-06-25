e8a3b5f9b408405be3c7cf7eeade264b8ea78f50
Jupieterrr <pieterteb@gmail.com>
2024-06-25 15:43:25 +0200

    wrote backbone of catalog object

--- CyPDF/include/cypdf_catalog.h
+++ CyPDF/include/cypdf_catalog.h
@@ -2,6 +2,8 @@
 #define CYPDF_CATALOG_H
 
 
+#include <stdio.h>
+
 #include "cypdf_dict.h"
 #include "cypdf_object.h"
 
@@ -9,8 +11,16 @@
 
 typedef struct _CYPDF_Catalog_Obj {
     CYPDF_Obj_Header        header;
-    CYPDF_Dict_Obj          dict;
+    CYPDF_Obj_Funcs         funcs;
+    CYPDF_Dict_Obj*         dict;
 } CYPDF_Catalog_Obj;
+
+
+CYPDF_Catalog_Obj* CYPDF_Catalog_Obj_New();
+
+void CYPDF_Catalog_Obj_Write(FILE* fp, CYPDF_Catalog_Obj* catalog);
+
+void CYPDF_Catalog_Obj_Free(CYPDF_Object* catalog);
 
 
 
--- CyPDF/src/cypdf_catalog.c
+++ CyPDF/src/cypdf_catalog.c
@@ -1,5 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "cypdf_catalog.h"
+#include "cypdf_consts.h"
+#include "cypdf_dict.h"
+#include "cypdf_doc.h"
+#include "cypdf_object.h"
 
 
 
-CYPDF_Catalog_Obj* CYPDF_Catalog_Obj_New();
+CYPDF_Catalog_Obj* CYPDF_Catalog_Obj_New(CYPDF_Doc* pdf, CYPDF_BOOL direct, CYPDF_BOOL indirect) {
+    CYPDF_Catalog_Obj* catalog = (CYPDF_Catalog_Obj*)CYPDF_Obj_New(pdf, direct, indirect, CYPDF_OCLASS_CATALOG);
+    catalog->funcs.write = CYPDF_Catalog_Obj_Write;
+    catalog->funcs.free = CYPDF_Catalog_Obj_Free;
+    catalog->dict = CYPDF_Dict_Obj_New(pdf, CYPDF_TRUE, CYPDF_FALSE);
+}
+
+void CYPDF_Catalog_Obj_Write(FILE* fp, CYPDF_Catalog_Obj* catalog) {
+    return;
+}
+
+void CYPDF_Catalog_Obj_Free(CYPDF_Object* obj) {
+    CYPDF_Catalog_Obj* _obj = (CYPDF_Catalog_Obj*)obj;
+    CYPDF_Obj_Free(_obj->dict);
+    free(_obj);
+}
