210e09670ec212480cbafd064e2593668ef25dc1
Enzo <sandrodegruijter@gmail.com>
2024-09-08 17:27:47 +0200

    Remove old, broken compile scripts

--- compile-and-run.sh
+++
@@ -1,6 +0,0 @@
-# Simple compilation bash script (no windows)
-# Made for rapid compiling and testing 
-
-gcc compile.c
-./a.out example.cyp
-rm ./a.out
--- run.sh
+++
@@ -1,6 +0,0 @@
-# Simple compilation bash script (no windows)
-# Made for rapid compiling and testing 
-
-gcc compile.c
-./a.out
-rm ./a.out
